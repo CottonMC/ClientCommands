e525692b23347135067b030397f97dc2fdf9c2aa
Juuxel <kasperi.kauppi@gmail.com>
2019-01-15 17:13:53 +0200

    Update error formatting

--- src/main/java/io/github/cottonmc/clientcommands/mixin/PlayerMixin.java
+++ src/main/java/io/github/cottonmc/clientcommands/mixin/PlayerMixin.java
@@ -6,6 +6,9 @@ import net.minecraft.client.network.ClientCommandSource;
 import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.command.CommandException;
 import net.minecraft.text.StringTextComponent;
+import net.minecraft.text.TextComponent;
+import net.minecraft.text.TextFormat;
+import net.minecraft.text.TranslatableTextComponent;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -14,8 +17,10 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ClientPlayerEntity.class)
-public class PlayerMixin {
+public abstract class PlayerMixin {
     @Shadow @Final protected MinecraftClient client;
+
+    @Shadow public abstract void addChatMessage(TextComponent textComponent_1, boolean boolean_1);
 
     @Inject(method = "sendChatMessage", at = @At("HEAD"), cancellable = true)
     private void onChatMessage(String msg, CallbackInfo info) {
@@ -30,10 +35,13 @@ public class PlayerMixin {
                 // Prevent sending the message
                 cancel = true;
         } catch (CommandException e) {
-            client.player.addChatMessage(e.getMessageComponent(), false);
+            addChatMessage(e.getMessageComponent().applyFormat(TextFormat.RED), false);
             cancel = true;
         } catch (CommandSyntaxException e) {
-            client.player.addChatMessage(new StringTextComponent(e.getRawMessage().getString()), false);
+            addChatMessage(new StringTextComponent(e.getMessage()).applyFormat(TextFormat.RED), false);
+            cancel = true;
+        } catch (Exception e) {
+            addChatMessage(new TranslatableTextComponent("command.failed").applyFormat(TextFormat.RED), false);
             cancel = true;
         }
 
