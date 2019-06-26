56dc9465c8bddccdc3b451ec3a7b74c7e676c8f7
joaoh1 <7559492+joaoh1@users.noreply.github.com>
2019-06-26 16:18:45 -0300

    Update to 1.14.3

--- src/main/java/io/github/cottonmc/clientcommands/mixin/ClientCommandSourceMixin.java
+++ src/main/java/io/github/cottonmc/clientcommands/mixin/ClientCommandSourceMixin.java
@@ -1,11 +1,11 @@
 package io.github.cottonmc.clientcommands.mixin;
 
 import io.github.cottonmc.clientcommands.CottonClientCommandSource;
-import net.minecraft.ChatFormat;
+import net.minecraft.util.Formatting;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientCommandSource;
-import net.minecraft.network.chat.Component;
-import net.minecraft.network.chat.TextComponent;
+import net.minecraft.text.Text;
+import net.minecraft.text.LiteralText;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -16,12 +16,12 @@ public abstract class ClientCommandSourceMixin implements CottonClientCommandSou
     private MinecraftClient client;
 
     @Override
-    public void sendFeedback(Component component) {
-        client.player.addChatMessage(component, false);
+    public void sendFeedback(Text text) {
+        client.player.addChatMessage(text, false);
     }
 
     @Override
-    public void sendError(Component component) {
-        client.player.addChatMessage(new TextComponent("").append(component).applyFormat(ChatFormat.RED), false);
+    public void sendError(Text text) {
+        client.player.addChatMessage(new LiteralText("").append(text).formatted(Formatting.RED), false);
     }
 }
