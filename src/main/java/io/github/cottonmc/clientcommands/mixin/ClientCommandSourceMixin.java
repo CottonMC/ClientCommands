a12efd9293cbe5ee377c301a9d4e1779c5b417f7
Guntram Blohm <gbl@guntram.de>
2020-06-20 22:16:33 +0200

    Make CCC work with Minecraft 1.16-rc1, fix #11

--- src/main/java/io/github/cottonmc/clientcommands/mixin/ClientCommandSourceMixin.java
+++ src/main/java/io/github/cottonmc/clientcommands/mixin/ClientCommandSourceMixin.java
@@ -1,11 +1,11 @@
 package io.github.cottonmc.clientcommands.mixin;
 
 import io.github.cottonmc.clientcommands.CottonClientCommandSource;
-import net.minecraft.util.Formatting;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientCommandSource;
-import net.minecraft.text.Text;
 import net.minecraft.text.LiteralText;
+import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -17,16 +17,16 @@ public abstract class ClientCommandSourceMixin implements CottonClientCommandSou
 
     @Override
     public void sendFeedback(Text message) {
-        client.player.addChatMessage(message, false);
+        client.player.sendMessage(message, false);
     }
 
     @Override
     public void sendFeedback(Text message, boolean actionBar) {
-        client.player.addChatMessage(message, actionBar);
+        client.player.sendMessage(message, actionBar);
     }
 
     @Override
     public void sendError(Text text) {
-        client.player.addChatMessage(new LiteralText("").append(text).formatted(Formatting.RED), false);
+        client.player.sendMessage(new LiteralText("").append(text).formatted(Formatting.RED), false);
     }
 }
